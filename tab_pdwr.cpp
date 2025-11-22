c44b586b1056434d4d164f45ea6ef9938a9c3ff3
myandrzej <98713973+myandrzej@users.noreply.github.com>
2025-11-22 16:57:35 +0100

    Create tab_pdwr.cpp

---
+++ tab_pdwr.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int tab[8];
+
+    cout << "Podaj 8 liczb: " << endl;
+    for (int i = 0; i < 8; i++) {
+        cin >> tab[i];
+    }
+
+    cout << "Liczby w odwrotnej kolejnocci: ";
+    for (int i = 7; i >= 0; i--) {
+        cout << tab[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
